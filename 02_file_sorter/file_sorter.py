73a1dd2eb55b85635cb9aaeafbdbd8f84a717485
Dmitry Ignatov <291763553+dmitry-ignatov@users.noreply.github.com>
2026-06-13 16:17:50 +0300

    Добавлен ввод файлов пользователем

--- 02_file_sorter/file_sorter.py
+++ 02_file_sorter/file_sorter.py
@@ -1,43 +1,53 @@
-files = [
-    "photo.jpg",
-    "song.mp3",
-    "document.pdf",
-    "archive.zip",
-    "unknown.xyz",
-    "song2.wav"
-]
 
+files = []
+
+
+while True:
+
+    name = input("Введите имя файла: ")
+    if name == "":
+        print("Пустое имя файла нельзя добавить")
+    elif name == "stop":
+        break
+    else:
+        files.append(name)
+
+    
+
+    
 img = []
 aud = []
 doc = []
 arc = []
 unk = []
 
+print()
+
 for file in files:
-   
+
     if file.endswith(".png") or file.endswith(".jpg"):
-        print(f"{file} — image\n")
+        print(f"{file} — image")
         img.append(file)
         
     elif file.endswith(".mp3") or file.endswith(".wav"):
-        print(f"{file} — audio\n")
+        print(f"{file} — audio")
         aud.append(file)
         
     elif file.endswith(".pdf") or file.endswith(".txt"):
-        print(f"{file} — document\n")
+        print(f"{file} — document")
         doc.append(file)      
 
     elif file.endswith(".zip") or file.endswith(".rar"):
-        print(f"{file} — archive\n")
+        print(f"{file} — archive")
         arc.append(file)
         
     else:
-        print(f"{file} — unknown\n")
+        print(f"{file} — unknown")
         unk.append(file)
         
 
 
-print(f"Images: {img}")
+print(f"\nImages: {img}")
 print(f"Audio: {aud}")
 print(f"Documents: {doc}")
 print(f"Archives: {arc}")
