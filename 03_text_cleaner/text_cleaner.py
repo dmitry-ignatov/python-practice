8a96bb2f4d9f7447e399e7e2c39e3a72b32de48c
Dmitry Ignatov <291763553+dmitry-ignatov@users.noreply.github.com>
2026-06-15 17:37:02 +0300

    Добавил проверку повторов без учёта регистра
    Повторяющиеся строки больше не добавляются.
    Проверка повторов выполняется без учёта регистра.

--- 03_text_cleaner/text_cleaner.py
+++ 03_text_cleaner/text_cleaner.py
@@ -3,17 +3,23 @@
 def get_texts():
 
     texts = []
+    texts_low = []
 
     while True:
 
         text = input("Введите текст: ")
+        
         clean_text = text.strip()
-
-        if clean_text.lower() == "stop":
+        check_text = clean_text.lower()
+        
+        if check_text == "stop":
             break
         elif not clean_text:
             print("Нужно ввести текст")
+        elif check_text in texts_low:
+            print("Такая строка уже есть")
         else:
+            texts_low.append(check_text)
             texts.append(clean_text)
     return texts
 
@@ -23,8 +29,7 @@ def show_texts(texts):
         print("Строки не добавлены")
     else:
         print(", ".join(texts))
-        print(f"Количество строк: {len(texts)}")
+        print(f"\nКоличество строк: {len(texts)}")
 
 texts = get_texts()
-
 show_texts(texts)
