01d0b30d9eec3a1d1e5a4d8eb46a4efa5adb75cc
Dmitry Ignatov <291763553+dmitry-ignatov@users.noreply.github.com>
2026-06-15 16:32:24 +0300

    Вынес ввод и вывод строк в функции

--- 03_text_cleaner/text_cleaner.py
+++ 03_text_cleaner/text_cleaner.py
@@ -1,20 +1,30 @@
 
 
-texts = []
+def get_texts():
 
-while True:
+    texts = []
 
-    text = input("Введите текст: ")
-    clean_text = text.strip()
+    while True:
 
-    if clean_text.lower() == "stop":
-        break
-    elif not clean_text:
-        print("Нужно ввести текст")
+        text = input("Введите текст: ")
+        clean_text = text.strip()
+
+        if clean_text.lower() == "stop":
+            break
+        elif not clean_text:
+            print("Нужно ввести текст")
+        else:
+            texts.append(clean_text)
+    return texts
+
+
+def show_texts(texts):
+    if not texts:
+        print("Строки не добавлены")
     else:
-        texts.append(clean_text)
+        print(", ".join(texts))
+        print(f"Количество строк: {len(texts)}")
 
-if not texts:
-    print("Строки не добавлены")
-else:
-    print(texts)
+texts = get_texts()
+
+show_texts(texts)
