da31deb323b75684e792f0546dae429e5ed2583c
Dmitry Ignatov <291763553+dmitry-ignatov@users.noreply.github.com>
2026-06-20 20:34:20 +0300

    Добавлена группировка файлов по категориям

--- 04_folder_reporter/folder_reporter.py
+++ 04_folder_reporter/folder_reporter.py
@@ -4,16 +4,16 @@ from pathlib import Path
 def get_category(extension):
     if extension == ".png" or extension == ".jpg":
         return "image"
-        
+    
     elif extension == ".mp3" or extension == ".wav":
         return "audio"
-        
+    
     elif extension == ".pdf" or extension == ".txt":
         return "document"
 
     elif extension == ".zip" or extension == ".rar":
         return "archive"
-        
+    
     else:
         return "unknown"
     
@@ -27,18 +27,35 @@ def show_path_info(path, folder_path):
         print("Это файл, а не папка")
     elif folder_path.is_dir():
         print("Содержимое папки: ")
+
+        categories = {
+            "image": [],
+            "audio": [],
+            "document": [],
+            "archive": [],
+            "unknown": []
+        }
         file_count = 0
         folder_count = 0
+        
         for index, item in enumerate(folder_path.iterdir(), start=1):
             if item.is_file():
                 extension = item.suffix.lower()
                 category = get_category(extension)
+                categories[category].append(item.name)
                 file_count += 1
                 print(f"{index}. {item.name} — файл, расширение: {extension}, категория: {category}")
+                
             elif item.is_dir():
                 folder_count += 1
                 print(f"{index}. {item.name} — папка")
         print(f"\nФайлов: {file_count}\nПапок: {folder_count}")
+        print(f"\nКатегории файлов: ")
+        for category_name, file_list in categories.items():
+            print(f"{category_name}, количество: {len(file_list)}")
+            for file_name in file_list:
+                print(f"- {file_name}")
+            print("")
 
 
 path = input("Введите путь к папке: ")
