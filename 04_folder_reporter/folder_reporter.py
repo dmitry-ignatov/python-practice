fe464a39edcfc49461bcb6e44cf61c7ab017cb50
Dmitry Ignatov <291763553+dmitry-ignatov@users.noreply.github.com>
2026-06-21 17:24:19 +0300

    Улучшена структура программы и добавлена функция main

--- 04_folder_reporter/folder_reporter.py
+++ 04_folder_reporter/folder_reporter.py
@@ -43,60 +43,68 @@ def save_file(texts, filename):
     print(f"\nТекст сохранен в {filename}")
 
 
-def show_path_info(path, folder_path):
-    print(f"\nВы выбрали путь: {path}\n")
+def get_report(path, folder_path):
+    report_lines = []
+    report_lines.append(f"Выбранный путь: {path}")
+    report_lines.append("")
+    report_lines.append("Содержимое папки:")
 
+    categories = {
+        "image": [],
+        "audio": [],
+        "document": [],
+        "archive": [],
+        "unknown": []
+    }
+
+    file_count = 0
+    folder_count = 0       
+    for index, item in enumerate(folder_path.iterdir(), start=1):
+        if item.is_file():
+            extension = item.suffix.lower()
+            category = get_category(extension)
+            categories[category].append(item.name)
+            file_count += 1
+            report_lines.append(f"{index}. {item.name} — файл, расширение: {extension}, категория: {category}")
+            
+        elif item.is_dir():
+            folder_count += 1
+            report_lines.append(f"{index}. {item.name} — папка")
+    
+    report_lines.append("")
+    report_lines.append(f"Файлов: {file_count}")
+    report_lines.append(f"Папок: {folder_count}")
+    report_lines.append("")
+    report_lines.append("Категории файлов:")
+    for category_name, file_list in categories.items():
+        report_lines.append(f"{category_name}, количество: {len(file_list)}")
+        for file_name in file_list:
+            report_lines.append(f"- {file_name}")
+        report_lines.append("")
+    print("\n".join(report_lines))
+    return report_lines
+
+
+def show_path_info(path, folder_path):
     if not folder_path.exists():
         print("Такого пути нет")
     elif folder_path.is_file():
         print("Это файл, а не папка")
     elif folder_path.is_dir():
-        report_lines = []
-        report_lines.append(f"Выбранный путь: {path}")
-        report_lines.append("")
-        report_lines.append("Содержимое папки:")
-
-        categories = {
-            "image": [],
-            "audio": [],
-            "document": [],
-            "archive": [],
-            "unknown": []
-        }
-
-        file_count = 0
-        folder_count = 0       
-        for index, item in enumerate(folder_path.iterdir(), start=1):
-            if item.is_file():
-                extension = item.suffix.lower()
-                category = get_category(extension)
-                categories[category].append(item.name)
-                file_count += 1
-                report_lines.append(f"{index}. {item.name} — файл, расширение: {extension}, категория: {category}")
-                
-            elif item.is_dir():
-                folder_count += 1
-                report_lines.append(f"{index}. {item.name} — папка")
-        
-        report_lines.append("")
-        report_lines.append(f"Файлов: {file_count}")
-        report_lines.append(f"Папок: {folder_count}")
-        report_lines.append("")
-        report_lines.append("Категории файлов:")
-        for category_name, file_list in categories.items():
-            report_lines.append(f"{category_name}, количество: {len(file_list)}")
-            for file_name in file_list:
-                report_lines.append(f"- {file_name}")
-            report_lines.append("")
-        print("\n".join(report_lines))
-        return report_lines
+        texts = get_report(path, folder_path)
+        return texts
 
 
-path = input("Введите путь к папке: ")
-folder_path = Path(path)
+def main():
+    path = input("Введите путь к папке: ")
+    folder_path = Path(path)
 
-texts = show_path_info(path, folder_path)
-if texts:
-    filename = get_filename()
-    save_file(texts, filename)
+    texts = show_path_info(path, folder_path)
+    if texts:
+        filename = get_filename()
+        save_file(texts, filename)
+
+
+if __name__ == "__main__":
+    main()
 
