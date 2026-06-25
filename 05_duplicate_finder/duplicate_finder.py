04863205f2784dbe3f8fc2a7665a98bbdf1954a4
Dmitry Ignatov <291763553+dmitry-ignatov@users.noreply.github.com>
2026-06-25 17:39:35 +0300

    Добавлен поиск кандидатов на дубликаты
    Файлы сгруппированы по размеру с помощью словаря.
    Программа выводит группы из двух и более файлов одинакового размера.
    Добавлено сообщение, если кандидаты на дубликаты не найдены.

--- 05_duplicate_finder/duplicate_finder.py
+++ 05_duplicate_finder/duplicate_finder.py
@@ -6,6 +6,30 @@ folder_path = Path(path)
 if not folder_path.exists():
     print("Такого пути нет")
 elif not folder_path.is_dir():
+    print("Путь не является папкой")
+else:
+    files = {}
+    
     for item in folder_path.iterdir():
+
         if item.is_file():
-            print(f"{item.name}, {item.stat().st_size}")
+            file_size = item.stat().st_size
+
+            if file_size not in files:
+                files[file_size] = []
+            files[file_size].append(item)
+
+    duplicates_found = False
+    for file_size, file_paths in files.items():
+        if len(file_paths) >= 2:
+            print(f"Размер файлов: {file_size} байт")
+            print("Кандидаты на дубликаты:")
+            for file_path in file_paths:
+                print(file_path.name)
+            duplicates_found = True
+            print()
+    if not duplicates_found:
+        print("Файлы с одинаковым размером не найдены")
+
+                    
+
