6fc644e6b9d035680cd0319460428d6f4670b5c4
Dmitry Ignatov <291763553+dmitry-ignatov@users.noreply.github.com>
2026-06-26 15:48:32 +0300

    Код разделён на функции

--- 05_duplicate_finder/duplicate_finder.py
+++ 05_duplicate_finder/duplicate_finder.py
@@ -2,7 +2,16 @@ from pathlib import Path
 import hashlib
 
 
+def get_path():
+
+    path = input("Введите путь к папке: ")
+    folder_path = Path(path)
+    print()
+    return folder_path
+
+
 def get_hash(file_path):
+
     with file_path.open("rb") as file:
         file_data = file.read()
 
@@ -11,17 +20,10 @@ def get_hash(file_path):
     result = hash_object.hexdigest()
     return result
 
-path = input("Введите путь к папке: ")
-folder_path = Path(path)
 
-if not folder_path.exists():
-    print("Такого пути нет")
-elif not folder_path.is_dir():
-    print("Путь не является папкой")
-else:
-    print()
+def get_files_by_size(folder_path):
+
     files_by_size = {}
-    
     for file_path in folder_path.iterdir():
 
         if file_path.is_file():
@@ -30,27 +32,50 @@ else:
             if file_size not in files_by_size:
                 files_by_size[file_size] = []
             files_by_size[file_size].append(file_path)
+    return files_by_size
+
+
+def get_files_by_hash(file_paths):
+   
+    files_by_hash = {}
+    for file_path in file_paths:
+        file_hash = get_hash(file_path)
+
+        if file_hash not in files_by_hash:
+            files_by_hash[file_hash] = []
+        files_by_hash[file_hash].append(file_path.name)
+    return files_by_hash
+    
+
+def get_duplicates(files_by_size):
 
     duplicates_found = False
-    for file_size, file_paths in files_by_size.items():
+    for file_paths in files_by_size.values():
         if len(file_paths) >= 2:
+            files_by_hash = get_files_by_hash(file_paths)
 
-            files_by_hash = {}
-            for file_path in file_paths:
-                file_hash = get_hash(file_path)
-
-                if file_hash not in files_by_hash:
-                    files_by_hash[file_hash] = []
-                files_by_hash[file_hash].append(file_path.name)
-
-            for _, file_names in files_by_hash.items():
+            for file_names in files_by_hash.values():
                 if len(file_names) >= 2:
                     duplicates_found = True
                     print(f"{', '.join(file_names)} - дубликаты")
-   
+
     print()
     if not duplicates_found:
         print("Одинаковые файлы не найдены")
 
+def main():
+
+    folder_path = get_path()
+    if not folder_path.exists():
+        print("Такого пути нет")
+    elif not folder_path.is_dir():
+        print("Путь не является папкой")
+    else:   
+        files_by_size = get_files_by_size(folder_path)
+        get_duplicates(files_by_size)
+
+
+if __name__ == "__main__":
+    main()
                     
 
