dbba32e569af6f97a152a2a145516bb826788662
Dmitry Ignatov <291763553+dmitry-ignatov@users.noreply.github.com>
2026-06-29 16:25:19 +0300

    Добавлена отметка выполненных задач

--- 06_personal_task_tracker/personal_task_tracker.py
+++ 06_personal_task_tracker/personal_task_tracker.py
@@ -9,7 +9,7 @@ tasks = []
 
 while True:
 
-    print("1. Добавить задачу\n2. Показать задачи\n0. Выход\n")
+    print("1. Добавить задачу\n2. Показать задачи\n3. Отметить задачу выполненной\n0. Выход\n")
 
     try:
         command = int(input("Выберите команду: "))
@@ -17,9 +17,10 @@ while True:
         print("Неверное значение введите число\n")
         continue
 
-    if command > 2 or command < 0:
+    if command > 3 or command < 0:
         print("Такой команды нет\n")
         continue
+
 
     elif command == 1:
         while True:
@@ -33,7 +34,8 @@ while True:
                 task_info["done"] = False
                 tasks.append(task_info)
                 break
-    
+
+
     elif command == 2 and not tasks:
         print("Задачи не введены")
     elif command == 2:
@@ -45,6 +47,31 @@ while True:
                 status = "[ ]"
             print(f"{index}. {status} {task['title']} ")
         print()
+
+
+    elif command == 3 and not tasks:
+        print("Задачи не введены")
+    elif command == 3:
+
+        for index, task in enumerate(tasks, start=1):
+            if task["done"]:
+                status = "[x]"
+            else:
+                status = "[ ]"
+            print(f"{index}. {status} {task['title']} ")
+        while True:
+            try:
+                task_number = int(input("Выберите номер задачи: "))
+            except ValueError:
+                print("Неверное значение введите число\n")
+                continue
+
+            if task_number > len(tasks) or task_number <= 0:
+                print("Номера такой задачи нет")
+            else:
+                tasks[task_number - 1]["done"] = True
+                print("Выбранная задача отмечена\n")
+                break
 
     elif command == 0:
         break
