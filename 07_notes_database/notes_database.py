9e05b47d91b775149f59035f45bf45df516ef4e4
Dmitry Ignatov <291763553+dmitry-ignatov@users.noreply.github.com>
2026-07-01 18:45:03 +0300

    Добавлено создание и отображение заметок

--- 07_notes_database/notes_database.py
+++ 07_notes_database/notes_database.py
@@ -1,15 +1,31 @@
 import sqlite3
 
+def normalize_text(text):
+    words = text.split()
+    text_normalized = " ".join(words)
+    return text_normalized
 
 
-connection = sqlite3.connect("notes.db")
+note_text = input("Введите заметку: ")
+note_text_normalized = normalize_text(note_text)
 
-connection.execute(""" 
-                   CREATE TABLE IF NOT EXISTS notes (
-                   id INTEGER PRIMARY KEY AUTOINCREMENT, 
-                   text TEXT
-                   ) 
-                   """)
+if not note_text_normalized:
+    print("Введена пустая строка")
+else:
+    connection = sqlite3.connect("notes.db")
+    connection.execute(""" 
+                    CREATE TABLE IF NOT EXISTS notes (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    text TEXT
+                    ) 
+                    """)
 
-connection.commit()
-connection.close()
+    connection.execute("INSERT INTO notes (text) VALUES (?)", (note_text_normalized,))
+    connection.commit()
+
+    cursor = connection.execute("SELECT id, text FROM notes")
+    notes = cursor.fetchall()
+    connection.close()
+
+    for note_id, text in notes:
+        print(f"{note_id}. {text}")
