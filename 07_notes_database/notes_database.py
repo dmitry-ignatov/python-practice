3741825b47fcead3d05131934f5198e90915a3de
Dmitry Ignatov <291763553+dmitry-ignatov@users.noreply.github.com>
2026-07-02 16:38:17 +0300

    Добавлено удаление заметок

--- 07_notes_database/notes_database.py
+++ 07_notes_database/notes_database.py
@@ -8,6 +8,22 @@ def normalize_text(text):
     return text_normalized
 
 
+def show_notes(connection):
+    cursor = connection.execute("SELECT id, text FROM notes")
+    notes = cursor.fetchall()
+    empty_notes = False
+    if not notes:
+        print("Заметок пока нет\n")
+        empty_notes = True
+        return empty_notes
+    else:
+        print("Текущие заметки: ")
+        for note_id, text in notes:
+            print(f"{note_id}. {text}")
+        print()
+        return False
+
+
 connection = sqlite3.connect("notes.db")
 connection.execute(""" 
                     CREATE TABLE IF NOT EXISTS notes (
@@ -19,16 +35,17 @@ connection.commit()
 
 while True:
 
-    print("1. Добавить заметку\n2. Показать заметки\n0. Выход\n")
+    print("1. Добавить заметку\n2. Показать заметки\n3. Удалить заметку\n0. Выход\n")
     try:
         command = int(input("Введите команду: "))
     except ValueError:
         print("Введите номер команды\n")
         continue
 
-    if command > 2 or command < 0:
+    if command > 3 or command < 0:
         print("Такой команды нет\n")
-        
+
+
     elif command == 1:
         note_text = input("Введите заметку: ")
         note_text_normalized = normalize_text(note_text)
@@ -42,16 +59,30 @@ while True:
 
 
     elif command == 2:
-        cursor = connection.execute("SELECT id, text FROM notes")
-        notes = cursor.fetchall()
-        if not notes:
-            print("Заметок пока нет\n")
-        else:
-            for note_id, text in notes:
-                print(f"{note_id}. {text}")
-            print()
-
+        show_notes(connection)
     
+    elif command == 3:
+        empty_notes = show_notes(connection)
+
+        if not empty_notes:
+            while True:
+                try:
+                    note_id = int(input("Введите номер заметки для удаления: "))
+                    if note_id <= 0:
+                        print("Неверный номер заметки")
+                        continue
+                    cursor = connection.execute("DELETE FROM notes WHERE id = ?", (note_id,))
+                    if cursor.rowcount == 0:
+                        print("Неверный номер заметки")
+                    else:
+                        connection.commit()
+                        print("Заметка удалена\n")
+                        break
+                except ValueError:
+                    print("\nВведите число\n")
+                    continue
+
+
     elif command == 0:
         connection.close()
         break
