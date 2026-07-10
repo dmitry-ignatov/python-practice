b3df23331289679276f24cd5ba1dd99dd80bc752
Dmitry Ignatov <291763553+dmitry-ignatov@users.noreply.github.com>
2026-07-10 17:31:44 +0300

    Добавлены рекомендации к усилению пароля

--- 09_password_checker/password_checker.py
+++ 09_password_checker/password_checker.py
@@ -1,7 +1,7 @@
 
 
 
-def password_len(password):
+def get_password_len(password):
     if len(password) < 8:
         len_password = "короткий"
         score = 0
@@ -56,20 +56,62 @@ def is_there_spaces(password):
         return ["нет", score]
 
 
+def is_simple_password(password):
+    score = 1
+    simple_passwords = ["password", "qwerty", "qwerty123", "123456", "12345678", "111111", "admin", "admin123", "пароль", "йцукен"]
+    for simple_password in simple_passwords:
+        if simple_password == password.lower():
+            score = 0
+            return ["да", score]
+    return ["нет", score]
+
+
+def get_score_sum(letters_len, letters_size, letters_digit, letters_spec, letters_space, simple_password):
+    
+    score_sum = int(letters_len[1] + letters_size[2] + letters_digit[1] + letters_spec[1] + letters_space[1] + simple_password[1])
+    if score_sum <= 3:
+        assess = "слабый пароль"
+    elif score_sum >= 4 and score_sum <= 6:
+        assess = "средний пароль"
+    elif score_sum >= 7 and score_sum <= 10:
+        assess = "сильный пароль"
+    return assess
+
+
+def recommendations(letters_len, letters_size, letters_digit, letters_spec, letters_space, simple_password):
+
+    if letters_len[0] == "короткий":
+        print("- Увеличьте длину пароля")
+    elif letters_len[0] == "нормальный":
+        print("- Немного увеличьте длину пароля")
+    
+    if letters_size[0] == "нет":
+        print("- Добавьте хотя бы одну большую букву")
+    if letters_size[1] == "нет":
+        print("- Добавьте хотя бы одну маленькую букву")
+
+    if letters_digit[0] == "нет":
+        print("- Добавьте хотя бы одну цифру")
+    
+    if letters_spec[0] == "нет":
+        print("- Добавьте хотя бы один спецсимвол")
+
+    if letters_space[0] == "есть":
+        print("- Уберите пробел")
+    
+    if simple_password[0] == "да":
+        print("- Избегайте простых паролей")
+
+    
 password = input("Введите пароль: ")
-letters_len = password_len(password)
+letters_len = get_password_len(password)
 letters_size = small_or_big_letter(password)  
 letters_digit = is_there_digit(password)
 letters_spec = is_spec_letter(password)
-letters_space =is_there_spaces()
+letters_space = is_there_spaces(password)
+simple_password = is_simple_password(password)
+assess = get_score_sum(letters_len, letters_size, letters_digit, letters_spec, letters_space, simple_password)
 
-score_sum = int(letters_len[1] + letters_size[2] + letters_digit[1] + letters_spec[1] + letters_space[1])
-if score_sum <= 3:
-    assess = "слабый пароль"
-elif score_sum >= 4 and score_sum <= 6:
-    assess = "средний пароль"
-elif score_sum >= 7 and score_sum <= 10:
-    assess = "сильный пароль"
 
 print(f"""Результат проверки:
 Длина: {letters_len[0]}
@@ -77,5 +119,10 @@ print(f"""Результат проверки:
 Маленькие буквы: {letters_size[1]}
 Цифры: {letters_digit[0]}
 Спецсимволы: {letters_spec[0]}
-Пробелы: {letters_space[0]}""")
+Пробелы: {letters_space[0]}
+Простой пароль: {simple_password[0]}""")
+
 print(f"\nОценка: {assess}\n")
+
+print("Рекомендации:")
+recommendations(letters_len, letters_size, letters_digit, letters_spec, letters_space, simple_password)
