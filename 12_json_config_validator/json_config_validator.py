8b34729c5bf168cce5d45c53c8a33a50461f32ad
Dmitry Ignatov <291763553+dmitry-ignatov@users.noreply.github.com>
2026-07-24 18:00:50 +0300

    Добавлена проверка типов полей конфигурации

--- 12_json_config_validator/json_config_validator.py
+++ 12_json_config_validator/json_config_validator.py
@@ -17,6 +17,23 @@ def get_json(json_path):
         print("Некорректный формат JSON")
 
 
+def get_incorrect_type_fields(data):
+    incorrect_fields = []
+    if "app_name" in data and not isinstance(data["app_name"], str):
+        incorrect_fields.append("app_name")
+
+    if "debug" in data and not isinstance(data["debug"], bool):
+        incorrect_fields.append("debug")
+
+    if "max_users" in data and type(data["max_users"]) is not int:
+        incorrect_fields.append("max_users")
+
+    if "log_level" in data and not isinstance(data["log_level"], str):
+        incorrect_fields.append("log_level")
+
+    return incorrect_fields
+
+
 def get_missing_fields(data):
     required_fields = ["app_name", "debug", "max_users", "log_level"]
     missing_fields = []
@@ -26,14 +43,23 @@ def get_missing_fields(data):
     return missing_fields
 
 
+def show_fields_errors(missing_fields, incorrect_fields):
+    
+    if missing_fields:
+        print(f"В JSON файле отсутствуют поля: {missing_fields}")
+    
+    if incorrect_fields:
+        print(f"В JSON файле неправильный тип данных у: {incorrect_fields}")
+
+
 json_path = Path(__file__).parent / "config.json"
 data = get_json(json_path)
 if data is not None:
     if is_dict(data):
         print(data)
         missing_fields = get_missing_fields(data)
-        if missing_fields:
-            print(missing_fields)
+        incorrect_fields = get_incorrect_type_fields(data)
+        show_fields_errors(missing_fields, incorrect_fields)
     else:
         print("Содержимое JSON не является словарем")
 
