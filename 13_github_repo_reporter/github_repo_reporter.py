7b6fbf824f33ee06694428ae9c666dd75d4791e9
Dmitry Ignatov <291763553+dmitry-ignatov@users.noreply.github.com>
2026-07-31 20:22:40 +0300

    Добавлено сохранение отчёта в файл

--- 13_github_repo_reporter/github_repo_reporter.py
+++ 13_github_repo_reporter/github_repo_reporter.py
@@ -1,5 +1,6 @@
 import requests
 from datetime import datetime
+from pathlib import Path
 
 
 def input_repository_name():
@@ -34,22 +35,58 @@ def get_repository_response(repo_full_name):
         return
 
 
-repo_name = input_repository_name()
-result = get_repository_response(repo_name)
-if result is not None:
-    if result.status_code == 200:
-        repository_data = result.json()
-        print(f"Название: {repository_data['name']}")
-        print(f"Полное имя: {repository_data['full_name']}")
-        print(f"Описание: {repository_data['description']}")
-        print(f"Основной язык: {repository_data['language']}")
-        print(f"Количество звёзд: {repository_data['stargazers_count']}")
-        print(f"Количество форков: {repository_data['forks_count']}")
-        print(f"Количество открытых задач: {repository_data['open_issues_count']}")
-        date_time = datetime.strptime(repository_data['updated_at'], "%Y-%m-%dT%H:%M:%SZ")
-        formatted_datetime = date_time.strftime("%d.%m.%Y %H:%M:%S")
-        print(f"Последнее обновление: {formatted_datetime}")
-    elif result.status_code == 404:
-        print("Репозиторий не найден")
+def create_repository_report(repository_data):
+    repository_report = [f"Название: {repository_data['name']}", 
+                         f"Полное имя: {repository_data['full_name']}"
+                    ]
+    if repository_data['description'] is None:
+        repository_report.append("Описание отсутствует")
     else:
-        print(f"Код ошибки: {result.status_code}")
+        repository_report.append(f"Описание: {repository_data['description']}")
+    if repository_data['language'] is None:
+        repository_report.append("Основной язык не определён")
+    else:
+        repository_report.append(f"Основной язык: {repository_data['language']}")
+    repository_report.append(f"Количество звёзд: {repository_data['stargazers_count']}")
+    repository_report.append(f"Количество форков: {repository_data['forks_count']}")
+    repository_report.append(f"Количество открытых задач: {repository_data['open_issues_count']}")
+    date_time = datetime.strptime(repository_data['updated_at'], "%Y-%m-%dT%H:%M:%SZ")
+    formatted_datetime = date_time.strftime("%d.%m.%Y %H:%M:%S")
+    repository_report.append(f"Последнее обновление: {formatted_datetime}")
+
+    return "\n".join(repository_report)
+
+
+def save_repository_report(repository_report):
+    try:
+        path_for_save_file = Path(__file__).resolve().parent / "repository_report.txt"
+        with open(path_for_save_file, "w", encoding="utf-8") as file:
+            file.write(repository_report)
+        print(f"Отчёт сохранен в: {path_for_save_file}")
+    except PermissionError:
+        print("Доступ к файлу запрещён")
+    except OSError:
+        print("Не удалось сохранить отчёт")
+
+
+def show_repository_report(repository_report):
+    print(repository_report)
+
+
+def main():
+    repo_name = input_repository_name()
+    result = get_repository_response(repo_name)
+    if result is not None:
+        if result.status_code == 200:
+            repository_data = result.json()
+            repository_report = create_repository_report(repository_data)
+            show_repository_report(repository_report)
+            save_repository_report(repository_report)
+        elif result.status_code == 404:
+            print("Репозиторий не найден")
+        else:
+            print(f"Код ошибки: {result.status_code}")
+
+
+if __name__ == "__main__":
+    main()
