703559e711b80edfcb737a687668f55ecbd7e684
Dmitry Ignatov <291763553+dmitry-ignatov@users.noreply.github.com>
2026-07-31 18:24:41 +0300

    Добавлено формирование вывода о репозитории

--- 13_github_repo_reporter/github_repo_reporter.py
+++ 13_github_repo_reporter/github_repo_reporter.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 
 def get_repository_response(repo_full_name):
@@ -13,8 +14,16 @@ def get_repository_response(repo_full_name):
 result = get_repository_response("psf/requests")
 if result.status_code == 200:
     repository_data = result.json()
-    print(repository_data["name"])
-    print(repository_data["full_name"])
-    print(repository_data["description"])
+    print(f"Название: {repository_data['name']}")
+    print(f"Полное имя: {repository_data['full_name']}")
+    print(f"Описание: {repository_data['description']}")
+    print(f"Основной язык: {repository_data['language']}")
+    print(f"Количество звёзд: {repository_data['stargazers_count']}")
+    print(f"Количество форков: {repository_data['forks_count']}")
+    print(f"Количество открытых задач: {repository_data['open_issues_count']}")
+    date_time = datetime.strptime(repository_data['updated_at'], "%Y-%m-%dT%H:%M:%SZ")
+    format_datetime = datetime.strftime(date_time, "%d.%m.%Y %H:%M:%S")
+    print(f"Последнее обновление: {format_datetime}")
+
 else:
-    print(f"код ошибки {result.status_code}")
+    print(f"Код ошибки {result.status_code}")
