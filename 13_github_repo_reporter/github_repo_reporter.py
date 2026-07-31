f023ab127eaefc6b89cce1be41ce51e87fe7feb1
Dmitry Ignatov <291763553+dmitry-ignatov@users.noreply.github.com>
2026-07-31 19:31:09 +0300

    Добавлена обработка сетевых ошибок

--- 13_github_repo_reporter/github_repo_reporter.py
+++ 13_github_repo_reporter/github_repo_reporter.py
@@ -19,29 +19,37 @@ def input_repository_name():
 
 
 def get_repository_response(repo_full_name):
-    response = requests.get(
-        f"https://api.github.com/repos/{repo_full_name}",
-        timeout=10
-    )
+    try:
+        response = requests.get(
+            f"https://api.github.com/repos/{repo_full_name}",
+            timeout=10
+        )
 
-    return response
+        return response
+    except requests.Timeout:
+        print("Превышено время ожидания")
+        return
+    except requests.RequestException:
+        print("Ошибка соединения")
+        return
 
 
 repo_name = input_repository_name()
 result = get_repository_response(repo_name)
-if result.status_code == 200:
-    repository_data = result.json()
-    print(f"Название: {repository_data['name']}")
-    print(f"Полное имя: {repository_data['full_name']}")
-    print(f"Описание: {repository_data['description']}")
-    print(f"Основной язык: {repository_data['language']}")
-    print(f"Количество звёзд: {repository_data['stargazers_count']}")
-    print(f"Количество форков: {repository_data['forks_count']}")
-    print(f"Количество открытых задач: {repository_data['open_issues_count']}")
-    date_time = datetime.strptime(repository_data['updated_at'], "%Y-%m-%dT%H:%M:%SZ")
-    formatted_datetime = date_time.strftime("%d.%m.%Y %H:%M:%S")
-    print(f"Последнее обновление: {formatted_datetime}")
-elif result.status_code == 404:
-    print("Репозиторий не найден")
-else:
-    print(f"Код ошибки: {result.status_code}")
+if result is not None:
+    if result.status_code == 200:
+        repository_data = result.json()
+        print(f"Название: {repository_data['name']}")
+        print(f"Полное имя: {repository_data['full_name']}")
+        print(f"Описание: {repository_data['description']}")
+        print(f"Основной язык: {repository_data['language']}")
+        print(f"Количество звёзд: {repository_data['stargazers_count']}")
+        print(f"Количество форков: {repository_data['forks_count']}")
+        print(f"Количество открытых задач: {repository_data['open_issues_count']}")
+        date_time = datetime.strptime(repository_data['updated_at'], "%Y-%m-%dT%H:%M:%SZ")
+        formatted_datetime = date_time.strftime("%d.%m.%Y %H:%M:%S")
+        print(f"Последнее обновление: {formatted_datetime}")
+    elif result.status_code == 404:
+        print("Репозиторий не найден")
+    else:
+        print(f"Код ошибки: {result.status_code}")
