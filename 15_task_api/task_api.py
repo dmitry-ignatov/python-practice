c9dc51217411390ff779d5a0a0fa5a01e1415df1
Dmitry Ignatov <291763553+dmitry-ignatov@users.noreply.github.com>
2026-08-12 13:01:26 +0300

    Добавлены создание, изменение и удаление задач

--- 15_task_api/task_api.py
+++ 15_task_api/task_api.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 
 app = FastAPI()
+
+class TaskCreate(BaseModel):
+    title: str
+
+
+class TaskUpdate(BaseModel):
+    title: str
+
 
 
 @app.get("/")
@@ -16,7 +25,7 @@ tasks = [
 
 
 @app.get("/tasks")
-def get_tasks(task_id: int):
+def get_tasks():
     return tasks
 
 
@@ -24,6 +33,47 @@ def get_tasks(task_id: int):
 def get_task(task_id: int):
     for task in tasks:
         if task_id == task["id"]:
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
+
+
+@app.post("/tasks", status_code=201)
+def create_task(task: TaskCreate):
+    if tasks:
+        created_task = {
+            "id": tasks[-1]["id"] + 1, "title": task.title
+        }
+    else:
+        created_task = {
+            "id": 1, "title": task.title
+        }
+
+    tasks.append(created_task)
+    return created_task
+
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    for task in tasks:
+        if task_id == task["id"]:
+            tasks.remove(task)
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
+
+
+@app.patch("/tasks/{task_id}")
+def update_task(task_id: int, task_update: TaskUpdate):
+    for task in tasks:
+        if task_id == task["id"]:
+            task["title"] = task_update.title
             return task
 
     raise HTTPException(
